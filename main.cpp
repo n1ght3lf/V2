79f824e4f46c25f5b11916a583fd5711135e57b2
n1ght3lf <mariusseniucas@gmail.com>
2024-02-14 22:42:35 +0200

    Updated read data and deletestudentdata, now it checks whether the inputs are correct for exam and homework.

--- main.cpp
+++ main.cpp
@@ -3,17 +3,7 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-/*
-Papildykite programą taip, kad ji veiktu ir tokiu atveju, kai namų darbų ir įrašų skaičiai
-(namų darbų - n, studentų skaičius - m) yra nežinomi iš anksto, t.y. tik įvedimo metu vartotojas nusprendžia kuomet jis jau įvedė visų namų darbų rezultatus ir kada jis
-baigią žmonių įvedimą. Šią dalį reikia realizuoti dviem būdais (atskiri failai), kur visus studentus ir kiekvieno iš jų darbų rezultatus saugant į:
-          C  masyvą (atskiras failas, visur turi būti naudojami tik C masyvai).
-          std::vector  tipo konteinerį (atskiras failas, visur turi būti naudojami tik std::vector konteineriai).
-Atliekant šią užduotį reikia sukurti du atskirus *.cpp failus ir C masyvams ir vector, tačiau toliau nuo 0.2 versijos naudosime tik vector versiją.
 
-Papildykite programą taip, kad būtų galimybė, jog mokinio gautieji balai už namų darbus bei egzaminą būtų generuojami atsitiktinai.
-
-Išleiskite pataisytą subrelizą (juodraštinis release failas) Github sistemoje su užvardinimu v0.1.*/
 struct nameGrade {
     string name;
     string surname;
@@ -30,15 +20,19 @@ void readData(nameGrade& student) {
     if (student.name == "0") return;
     cout << "Enter student's surname: ";
     getline(cin, student.surname);
-    cout << "Enter homework results, 0 to finish: ";
+    cout << "Enter homework results, 11 to finish: ";
     int size = 1; // Initial array size
     student.homework = new double[size];
-    int i = 0; //index
+    int i = 0;
     while (true) {
         double input;
         cin >> input;
-        if (input == 0) break;
-        if (i == size) { // If the array is full, resize it
+        if (input == 11) break;
+        if (input < 0 || input > 10) {
+            cout << "Enter a grade between 0 and 10 or 11 to cancel." << endl;
+            continue;
+        }
+        if (i == size) {
             size *= 2;
             double* newHomework = new double[size];
             for (int j = 0; j < i; ++j) {
@@ -51,9 +45,17 @@ void readData(nameGrade& student) {
         ++i;
     }
     student.n = i;
-    cout << "Enter exam result: ";
-    cin >> student.exam;
+    while (true) {
+        cout << "Enter exam result: ";
+        cin >> student.exam;
+        if (student.exam < 0 || student.exam > 10) {
+            cout << "Enter a result between 0 and 10." << endl;
+            continue;
+        }
+        break;
+    }
 }
+
 
 double calculateFinalAverage(nameGrade& student) {
     double sum = 0;
@@ -75,8 +77,9 @@ double calculateFinalMedian(nameGrade& student) {
     return 0.4 * median + 0.6 * student.exam;
 }
 
-void deleteStudentData(nameGrade& student){
-
+void deleteStudentData(nameGrade& student) {
+    delete[] student.homework;
+    student.homework = nullptr;
 }
 
 void printResults(nameGrade& student, bool useAverage) {
@@ -123,57 +126,3 @@ int main() {
     
     return 0;
 }
-/*
-int main()
-{
-  int StudentAmount, HomeWorkAmount;
-  bool medianOrAverage;
-  float Temporary = 0, B[HomeWorkAmount + 1];
-  nameGrade* A = new nameGrade[];
-  for (int i = 0; i < StudentAmount; i++)
-  {
-    cout << "Name, last name and all homework results:\n";
-    cin >> A[i].Name >> A[i].LastName;
-    for (int j = 0; j < HomeWorkAmount; j++)
-    {
-      cin >> B[j];
-      A[i].HomeWorkResults += B[j];
-    }
-    cout << "Exam results:\n";
-    cin >> A[i].ExamGrade;
-    B[HomeWorkAmount] = A[i].ExamGrade;
-    sort(B, B + HomeWorkAmount);
-    if (HomeWorkAmount + 1 % 2 == 0)
-    {
-      A[i].Medianaa = (B[HomeWorkAmount / 2] + B[(HomeWorkAmount / 2) + 1]) / 2;
-      cout << "Median" << A[i].Medianaa << endl;
-    }
-    else
-    {
-      A[i].Medianaa = B[HomeWorkAmount / 2];
-    }
-    A[i].HomeWorkResults /= HomeWorkAmount;
-  }
-  cout << "For median input 0, for average input 1 ";
-  cin >> medianOrAverage;
-  if (medianOrAverage == 1)
-  {
-    cout << setw(15) << "Pavardė" << setw(15) << "Vardas" << setw(25) << "Galutinis(vid.)\n"
-         << "--------------------------------------------------\n";
-    for (int i = 0; i < StudentAmount; i++)
-    {
-      Temporary = (A[i].HomeWorkResults * 0.4) + (A[i].ExamGrade * 0.6);
-      cout << setw(15) << A[i].LastName << setw(15) << A[i].Name << setw(15) << fixed << setprecision(2) << Temporary << endl;
-    }
-  }
-  else
-  {
-    cout << setw(15) << "Pavardė" << setw(15) << "Vardas" << setw(25) << "Galutinis(med.)\n"
-         << "--------------------------------------------------\n";
-    for (int i = 0; i < StudentAmount; i++)
-    {
-      cout << setw(15) << A[i].LastName << setw(15) << A[i].Name << setw(15) << fixed << setprecision(2) << A[i].Medianaa << endl;
-    }
-  }
-}
-*/
