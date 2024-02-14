584f2614855c3e7cdb02e431bdc323754225b047
n1ght3lf <mariusseniucas@gmail.com>
2024-02-14 14:56:49 +0200

    Deleted previous version, remade it. Struct done, input done, auto calculation for homework average.

--- Lab1OoP/main.cpp
+++ Lab1OoP/main.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
-struct nameGrade {
-  string name, lastName;
-  float results, averageGrade, medianGrade;
+/*
+    Sukurkite pirmą Github projektą ir jame šaką (branch) - v.pradinė.
+
+    Pagal aprašytus užduoties reikalavimus realizuokite programą, kuri nuskaito vartotojų įvedamus reikiamus duomenis (patogiausiai naudoti struktūrą):
+        string tipo: studento vardas ir pavardė;
+        int tipo: namų darbų tarpiniai rezultatai ir egzamino rezultatas;
+
+    Baigus duomenų įvedimą, suskaičiuoja galutinį balą ir juos pateikia į ekraną tokiu ar panašiu pavidalu (galutinis
+        apskaičiuotas balas pateikiamas dviejų skaičių po kablelio tikslumu):
+
+    Papildykite programą, kad vietoj vidurkio galutinio balo skaičiavimui būtų galima naudoti ir medianą.
+        Tuomet išvedimas (output’as) turėtų atrodyti panašiai į šį, kur tik vienas pasirinktas Galutinis (Vid.) arba Galutinis (Med.) yra išvedamas:
+
+    first subrelease v.pradinė
+    --------------------------
+    branch v0.1 (continue later)
+*/
+struct nameGrade
+{
+  string Name, LastName;
+  float HomeWorkResults, ExamGrade;
 };
 
-int main(){
-  ifstream in("import.txt");
-  int n=0;
-  cout<<"Įveskite kelių studentų rezultatus tikrinsite"<<endl;
-  while (1<2){
-    cin>>n;
-    if (n<50 && n>0) break;
-    if (n>50) cout<<"Skaičius per didelis"<<endl;
-    if (n<1) cout<<"Skaičius per mažas"<<endl;
+int main()
+{
+  int StudentAmount, HomeWorkAmount;
+  cin >> "Student Amount:" >> StudentAmount >> "Homework amount:" >> HomeWorkAmount;
+  nameGrade A[StudentAmount];
+  for (int i = 0; i < StudentAmount; i++)
+  {
+    cin >> "Name:" >> A[i].Name >> "Last Name:" >> A[i].LastName >> "All hw results:";
+    for (int i = 0; i < HomeWorkAmount; i++)
+    {
+      cin >> A[i].HomeWorkResults;
+    }
+    cin >> "Exam results:" >> A[i].ExamGrade;
   }
-  nameGrade person[n];
-  for (int i = 0 ; i < n ; i++){
-  }
-  return 0;
+  
 }
