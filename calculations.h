2ecb60e43301d3575bf80c8e01c9d618091b0eed
n1ght3lf <mariusseniucas@gmail.com>
2024-06-08 22:09:52 +0300

    calculations and functionality .h files updated

--- calculations.h
+++ calculations.h
@@ -2,21 +2,28 @@
 #define CALCULATIONS_H
 
 #include <vector>
-#include "student.h"
+#include <list>
+#include <algorithm> 
+#include <numeric>   
+#include "student.h" 
 #include "input.h"
 
-bool compareByFirstName(const Student &a, const Student &b);
+bool compareByFirstName(const Student& a, const Student& b);
 
-bool compareByLastName(const Student &a, const Student &b);
+bool compareByLastName(const Student& a, const Student& b);
 
-bool compareByGrade(const Student &a, const Student &b);
+bool compareByGrade(const Student& a, const Student& b);
 
-double calculateAverage(const std::vector<int> &homeworkResults);
+double calculateAverage(const std::vector<int>& homeworkResults);
 
-double calculateFinalGrade(const Student &data, bool Median);
+double calculateFinalGrade(const Student& data, bool Median);
 
 double calculateMedian(std::vector<int> homeworkResults);
 
-void sortStudents(std::vector<Student> &students, int criteria, int Median);
+template <typename Container>
+void sortStudents(Container& students, int criteria);
+
+template <>
+void sortStudents<std::list<Student>>(std::list<Student>& students, int criteria);
 
 #endif // CALCULATIONS_H
--- functionality.h
+++ functionality.h
@@ -2,19 +2,20 @@
 #define FUNCTIONALITY_H
 
 #include <string>
-#include <cstdlib>
+#include <cstdlib> 
 #include <vector>
-#include "student.h"
+#include "student.h" 
 #include "calculations.h"
 
+int getContainerTypeFromUser();
 int generateGrade();
 std::string generateName();
 std::string generateLastName();
-std::string isString(const std::string &prompt);
-int isGrade(const std::string &prompt);
-void testFiles(const std::vector<std::string> &filenames);
-double generateFile(int studentCount);
-void outputToTerminal(const std::vector<Student> &badStudents, const std::vector<Student> &goodStudents, bool Median);
-void outputToFile(const std::vector<Student> &students, size_t numberOfStudents, bool Median, const std::string &filename);
+std::string isString(const std::string& prompt);
+int isGrade(const std::string& prompt);
+void generateFile(int studentCount);
+void outputToTerminal(const std::vector<Student>& badStudents, const std::vector<Student>& goodStudents, bool Median);
+template <typename Container>
+void outputToFile(const Container& students, size_t numberOfStudents, bool Median, const std::string& filename);
 
 #endif // FUNCTIONALITY_H
