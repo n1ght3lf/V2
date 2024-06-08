a6d9ef5bdcb75dcc4232e693822a631a585825c8
n1ght3lf <mariusseniucas@gmail.com>
2024-06-08 21:06:19 +0300

    Changed calculations.cpp

--- calculations.cpp
+++ calculations.cpp
@@ -6,58 +6,21 @@ using namespace std;
 
 bool compareByFirstName(const Student &a, const Student &b)
 {
-    return a.firstName < b.firstName;
+    return a.getFirstName() < b.getFirstName();
 }
 
 bool compareByLastName(const Student &a, const Student &b)
 {
-    return a.lastName < b.lastName;
+    return a.getLastName() < b.getLastName();
 }
 
 bool compareByGrade(const Student &a, const Student &b)
 {
-    return calculateFinalGrade(a, false) < calculateFinalGrade(b, false);
+    return a.calculateFinalGrade(false) < b.calculateFinalGrade(false);
 }
 
-double calculateAverage(const vector<int> &homeworkResults)
-{
-    if (homeworkResults.empty())
-    {
-        return 0; // jei vektorius tuscias, grazina 0
-    }
-    double sum = 0;
-    for (int i = 0; i < homeworkResults.size(); i++)
-    {
-        sum += homeworkResults[i];
-    }
-    return sum / homeworkResults.size();
-}
-
-double calculateFinalGrade(const Student &data, bool Median)
-{
-    double homeworkGrade = Median ? calculateMedian(data.homeworkResults) : calculateAverage(data.homeworkResults); // jei Median = true, naudoja mediana, jei ne, naudoja vidurki
-    return 0.4 * homeworkGrade + 0.6 * data.examResults;
-}
-
-double calculateMedian(vector<int> homeworkResults)
-{
-    if (homeworkResults.empty())
-    {
-        return 0; // jei vektorius tuscias, grazina 0
-    }
-    sort(homeworkResults.begin(), homeworkResults.end()); // surusiuoja vektoriu didejimo tvarka
-    int size = homeworkResults.size();
-    if (size % 2 == 0)
-    {
-        return (homeworkResults[size / 2 - 1] + homeworkResults[size / 2]) / 2.0;
-    }
-    else
-    {
-        return homeworkResults[size / 2];
-    }
-}
-
-void sortStudents(vector<Student> &students, int criteria, int Median)
+template <typename Container>
+void sortStudents(Container &students, int criteria)
 {
     if (criteria == 1)
     {
@@ -69,14 +32,26 @@ void sortStudents(vector<Student> &students, int criteria, int Median)
     }
     else if (criteria == 3)
     {
-        if (Median == 1)
-        {
-            sort(students.begin(), students.end(), [](const Student &a, const Student &b)
-                 { return calculateFinalGrade(a, true) < calculateFinalGrade(b, true); });
-        }
-        else
-        {
-            sort(students.begin(), students.end(), compareByGrade);
-        }
+        sort(students.begin(), students.end(), compareByGrade);
     }
 }
+
+template <>
+void sortStudents<std::list<Student>>(std::list<Student> &students, int criteria)
+{
+    if (criteria == 1)
+    {
+        students.sort(compareByFirstName);
+    }
+    else if (criteria == 2)
+    {
+        students.sort(compareByLastName);
+    }
+    else if (criteria == 3)
+    {
+        students.sort(compareByGrade);
+    }
+}
+
+template void sortStudents<std::vector<Student>>(std::vector<Student> &, int);
+template void sortStudents<std::deque<Student>>(std::deque<Student> &, int);
