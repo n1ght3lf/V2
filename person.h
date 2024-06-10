3cd561219daf5692ab9b1126febf5978386451f8
n1ght3lf <mariusseniucas@gmail.com>
2024-06-10 22:32:16 +0300

    Made person.h

---
+++ person.h
@@ -0,0 +1,25 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <deque>
+#include <list>
+#include <istream>
+#include <ostream>
+#include <iomanip>
+#include <sstream>
+
+class Person {
+public:
+    virtual ~Person() = default; // destructor
+    // getters
+    virtual std::string getFirstName() const = 0; 
+    virtual std::string getLastName() const = 0; 
+    virtual std::string getName() const = 0;
+};
+
+#endif // PERSON_H
