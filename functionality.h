e2bd73ecd0ffaca29a71a86dd9016ac2e9c6af36
n1ght3lf <mariusseniucas@gmail.com>
2024-06-11 10:32:23 +0300

    Testing between vector and myvector added

--- functionality.h
+++ functionality.h
@@ -17,5 +17,11 @@ void generateFile(int studentCount);
 void outputToTerminal(const std::vector<Student>& badStudents, const std::vector<Student>& goodStudents, bool Median);
 template <typename Container>
 void outputToFile(const Container& students, size_t numberOfStudents, bool Median, const std::string& filename);
+template <typename T>
+size_t getCapacity(const std::vector<T>& container);
+template <typename T>
+size_t getCapacity(const MyVector<T>& container);
+template <typename Container>
+void testContainer(unsigned int sz, const std::string& containerName);
 
 #endif // FUNCTIONALITY_H
